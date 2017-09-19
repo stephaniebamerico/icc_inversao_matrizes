4c6984b0122d6ffa3a9e537209db7ee6b171766d
Talita <thcf16@inf.ufpr.br>
2017-09-19 19:39:24 -0300

    QUE
    Signed-off-by: Talita <thcf16@inf.ufpr.br>

--- sba16-thcf16/src/encontraMatrizInversa.c
+++ sba16-thcf16/src/encontraMatrizInversa.c
@@ -5,37 +5,44 @@ void substituicao_Lyb (MATRIZ L, MATRIZ *y);
 void troca (MATRIZ *matriz, unsigned int l1, unsigned int l2);
 int pivotamentoParcial (MATRIZ *matriz, unsigned int col);
 void retrosubstituicao (MATRIZ U, MATRIZ *Y);
-void retrosubstituicaoUxy (MATRIZ U, MATRIZ *y);
 
 int main (int argc, char const *argv[]) {
     srand( 20172 );
 
     MATRIZ matriz;
-    matriz.tam = 2; // 1 < tam < 32768
+    matriz.tam = 3; // 1 < tam < 32768
 
     if(! (matriz.dados = generateSquareRandomMatrix(matriz.tam)) ) {
         fprintf(stderr, "Erro ao alocar a matriz usando generateSquareRandomMatrix.\n");
         return 0;
     }
 
-    matriz.dados[pos(0, 0, matriz.tam)]=2;
-    matriz.dados[pos(0, 1, matriz.tam)]=1;
-    matriz.dados[pos(1, 0, matriz.tam)]=5;
-    matriz.dados[pos(1, 1, matriz.tam)]=3;
+        
+    matriz.dados[pos(0,0,matriz.tam)] =1;
+    matriz.dados[pos(0,1,matriz.tam)] =2;
+    matriz.dados[pos(0,2,matriz.tam)] =3;
+    matriz.dados[pos(1,0,matriz.tam)] =4;
+    matriz.dados[pos(1,1,matriz.tam)] =5;
+    matriz.dados[pos(1,2,matriz.tam)] =6;
+    matriz.dados[pos(2,0,matriz.tam)] =7;
+    matriz.dados[pos(2,1,matriz.tam)] =8;
+    matriz.dados[pos(2,2,matriz.tam)] =1;
 
-    imprimeMatriz(matriz);
-    
+    //imprimeMatriz(matriz);  
+
+       
     fatoracaoLU(&matriz);
-    printf("matriz depois de LU:\n");
-    imprimeMatriz(matriz);
+    printf("LU:..\n"); 
+    //imprimeMatriz(matriz);  
 
-    MATRIZ x;   
+    MATRIZ x;
     substituicao_Lyb(matriz, &x);
-    printf("x depois de Ly=b:\n");
+
+    printf("x\n");
     imprimeMatriz(x);
-    retrosubstituicaoUxy(matriz,&x);
-    printf("inv:\n");
-    imprimeMatriz(x);
+    retrosubstituicao(matriz,&x);
+    //printf("inv:\n");
+    //imprimeMatriz(x);
 
     return 0;
 }
@@ -43,7 +50,7 @@ int main (int argc, char const *argv[]) {
 void fatoracaoLU (MATRIZ *matriz) {
     double m;
     for (int col = 0; col < matriz->tam-1; ++col) {
-        pivotamentoParcial(matriz, col);            
+        //pivotamentoParcial(matriz, col);            
             for (int lin = col+1; lin < matriz->tam; ++lin) {
                 m = (matriz->dados[pos(lin, col, matriz->tam)]*1.0)/matriz->dados[pos(col, col, matriz->tam)];
                 // U
@@ -79,57 +86,77 @@ void troca (MATRIZ *matriz, unsigned int l1, unsigned int l2) {
     }
 }
 
-void retrosubstituicaoUxy (MATRIZ U, MATRIZ *y) {
-	double *b = NULL;
-	if(! (b = (double *) malloc(U.tam*sizeof(double))) ){
-		fprintf(stderr, "[retrosubstituicaoUxy]: impossível alocar b.\n");
-		return;
-	}
+void retrosubstituicao (MATRIZ U, MATRIZ *Y)
+{
+    printf("%d\n",Y );
+    imprimeMatriz(*Y);
+    unsigned int tam = U.tam;
+    //double *x = (double*)(calloc (tam, sizeof(double)));
+    double x[U.tam];
+    printf("%u %u\n",Y,x );
+    imprimeMatriz(*Y);
 
-	for (int sl = 0; sl < U.tam; ++sl) {
-		for (int i = 0; i < y->tam; ++i) {
-			b[i] = y->dados[pos(i, sl, y->tam)];
-		}
-		for (int lin = U.tam-1; lin >= 0; --lin) {
-			y->dados[pos(lin, sl, y->tam)] = b[lin];
+    
+    for (int i = 0; i < tam; ++i)
+    {
+        double soma = 0;
+        for (int k = 0; k < tam; ++k)
+        {
+            x[k] = Y->dados[pos(k,i,tam)];
+            Y->dados[pos(k,i,tam)] = 0;
+        }
+        
 
-			for (int col = lin+1; col < U.tam; ++col) {
-				y->dados[pos(lin, sl, y->tam)] = U.dados[pos(lin, col, U.tam)]*y->dados[pos(col, sl, y->tam)];
-			}
+        Y->dados[pos(tam-1,i,tam)] = (x[tam-1]*1.0)/U.dados[pos(tam-1, tam-1, tam)];
 
-			y->dados[pos(lin, sl, y->tam)] /= U.dados[pos(lin, lin, U.tam)];
-		}
-	}
+        for (int k = tam-1; k >= 0; --k)
+        {
+            soma = x[k];
+
+
+            for (int j = tam-1; j > k; --j)
+            {
+                soma -= U.dados[pos(k,j,tam)]*Y->dados[pos(j,i,tam)];
+            }
+            Y->dados[pos(k,i,tam)]=(soma*1.0)/U.dados[pos(k,k,tam)];
+        }
+
+        
+    }
+
 }
+/*
+int resolveSup (double a[N][N], double x[N], double b[N]) {
+    double soma = 0;
+    x[N-1] = b[N-1]/a[N-1][N-1];
+    for (int k = N-2; k >= 0; --k) {
+        soma = b[k];
 
+        for (int j = k; j < N; ++j)
+        {
+            soma-= a[k][j]*(x[j]);
+        }
+
+        
+        x[k]=(soma*1.0)/a[k][k];
+    }
+
+    
+    return 0;
+}
+*/
 void substituicao_Lyb (MATRIZ L, MATRIZ *y) {
-	y->tam = L.tam;
-	y->dados = (double *) calloc(y->tam, sizeof(double));
+    y->tam = L.tam;
+    y->dados = (double *) calloc(y->tam, sizeof(double));
 
-	for (int i = 0; i < y->tam; ++i) // inicia como uma matriz identidade
-		y->dados[pos(i, i, y->tam)] = 1;
+    for (int i = 0; i < y->tam; ++i) // inicia como uma matriz identidade
+        y->dados[pos(i, i, y->tam)] = 1;
 
-	for (int b = 0; b < y->tam-1; ++b) { // cada coluna da matriz é um vetor b para resolver o sistema
-		for (int lin = 1; lin < y->tam; ++lin) { // da segunda linha até até a diagonal principal
-			for (int col = b; col < lin; ++col) { // começa a partir da primeira valoração não nula (b)
-				y->dados[pos(lin, b, y->tam)] -= L.dados[pos(lin, col, L.tam)]*y->dados[pos(col, b, y->tam)];
-			}
-		}
-	}
-}
-
-double *residuo (MATRIZ A, MATRIZ inv_A) {
-	double *r = NULL;
-	if ( ! (r = (double *) malloc(A.tam*sizeof(double))) )
-    	return NULL;
-    // r = A * inv_A
-    for(int lin=0; lin < A.tam; lin++) 
-        for(col=0; col < A.tam; col++){ 
-        	somaprod=0;
-        	for(i=0; i < A.tam; i++)
-          		somaprod += A[pos(lin, i, A->tam)]*inv_A[A[pos(i, col, A->tam)]]; 
-        	r[pos(lin, col, A->tam)]=somaprod; 
-		}
-
-    return r;
+    for (int b = 0; b < y->tam-1; ++b) { // cada coluna da matriz é um vetor b para resolver o sistema
+        for (int lin = 1; lin < y->tam; ++lin) { // da segunda linha até até a diagonal principal
+            for (int col = b; col < lin; ++col) { // começa a partir da primeira valoração não nula (b)
+                y->dados[pos(lin, b, y->tam)] -= L.dados[pos(lin, col, L.tam)]*y->dados[pos(col, b, y->tam)];
+            }
+        }
+    }
 }
